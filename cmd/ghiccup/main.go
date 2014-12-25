90675e253d952e534f64c39d0a54fb161bd144bb
Fawad Halim <fawad@fawad.net>
2014-12-25 12:02:27 -0600

    Added dash of hdrhistogram

--- cmd/ghiccup/main.go
+++ cmd/ghiccup/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/codahale/hdrhistogram"
 	"github.com/fhalim/ghiccup"
+	"log"
 	"time"
 )
 
 const DEFAULT_POLLING_FREQUENCY = 1000000
 const DEFAULT_PAUSE_THRESHOLD = DEFAULT_POLLING_FREQUENCY
+const MAX_INTERESTING_LATENCY_MINUTES = 10
 
 func main() {
 	resolutionNs := flag.Int64("resolution", DEFAULT_POLLING_FREQUENCY, "Frequency in ns at which to test")
@@ -19,15 +22,17 @@ func main() {
 	duration := time.Duration(*resolutionNs) * time.Nanosecond
 	threshold := time.Duration(*resolutionNs+*thresholdNs) * time.Nanosecond
 
-	execute(duration, threshold)
+	hist := hdrhistogram.New(0, (time.Duration(MAX_INTERESTING_LATENCY_MINUTES) * time.Minute).Nanoseconds(), 5)
+	execute(duration, threshold, hist)
 }
 
-func execute(resolution time.Duration, threshold time.Duration) {
+func execute(resolution time.Duration, threshold time.Duration, hist *hdrhistogram.Histogram) {
 	for {
 		startTime := time.Now()
 		time.Sleep(resolution)
 		endTime := time.Now()
 		durationNs := endTime.Sub(startTime).Nanoseconds()
+		hist.RecordValue(durationNs)
 		if durationNs > threshold.Nanoseconds() {
 			fmt.Println(ghiccup.Marshall(ghiccup.HiccupInfo{
 				Timestamp:  time.Now().Format(time.RFC3339),
@@ -35,6 +40,7 @@ func execute(resolution time.Duration, threshold time.Duration) {
 				Threshold:  threshold.Nanoseconds(),
 				Duration:   durationNs,
 			}))
+			log.Println("Percentiles: 95:", hist.ValueAtQuantile(95), "50:", hist.ValueAtQuantile(50))
 		}
 	}
 }
--- cmd/icmphiccup/main.go
+++ cmd/icmphiccup/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/codahale/hdrhistogram"
 	"github.com/fhalim/ghiccup"
 	"github.com/tatsushid/go-fastping"
 	"log"
 	"net"
 	"time"
 )
+
+const MAX_INTERESTING_LATENCY_SECONDS = 60
 
 func main() {
 	interval := flag.Int("interval", 1, "Interval in seconds between pings")
@@ -19,7 +22,9 @@ func main() {
 
 	threshold := time.Duration(*rttThresholdMs) * time.Millisecond
 
-	p := initializePinger(*host, threshold)
+	hist := hdrhistogram.New(0, (time.Duration(MAX_INTERESTING_LATENCY_SECONDS) * time.Second).Nanoseconds(), 5)
+
+	p := initializePinger(*host, threshold, hist)
 
 	go startPinger(p, time.Second*time.Duration(*interval))
 
@@ -38,7 +43,7 @@ func startPinger(pinger *fastping.Pinger, frequency time.Duration) {
 		time.Sleep(frequency)
 	}
 }
-func initializePinger(host string, reportingThreshold time.Duration) *fastping.Pinger {
+func initializePinger(host string, reportingThreshold time.Duration, hist *hdrhistogram.Histogram) *fastping.Pinger {
 	p := fastping.NewPinger()
 	log.Println("Pinging host", host)
 	ra, err := net.ResolveIPAddr("ip4:icmp", host)
@@ -49,12 +54,14 @@ func initializePinger(host string, reportingThreshold time.Duration) *fastping.P
 	p.AddIPAddr(ra)
 	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
 		log.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
+		hist.RecordValue(rtt.Nanoseconds())
 		if rtt > reportingThreshold {
 			fmt.Println(ghiccup.Marshall(ghiccup.HiccupInfo{
 				Timestamp: time.Now().Format(time.RFC3339),
 				Threshold: reportingThreshold.Nanoseconds(),
 				Duration:  rtt.Nanoseconds(),
 			}))
+			log.Println("Percentiles: 95:", hist.ValueAtQuantile(95), "50:", hist.ValueAtQuantile(50))
 		}
 	}
 	p.OnIdle = func() {
